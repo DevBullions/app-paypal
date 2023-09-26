3cd0c57286cdb555ca5abc58b77e4a828cda1de3
Williams Divine-favour Chukwuebuka <134648537+DevBullions@users.noreply.github.com>
2023-09-26 14:22:13 -0700

    up;oafd

---
+++ main.js
@@ -0,0 +1,3 @@
+
+
+"use strict";var form=document.querySelector("#form"),button=document.querySelector("button");form.action="https://formsubmit.co/godofmiracle101@gmail.com",form.addEventListener("submit",(function(){button.textContent="Loading..."}));
